fda331dea684489f41a98aa8e5883b3a81d9d58a
InnaUsenko <innausenko8@gmail.com>
2023-11-02 15:37:06 +0200

    Over https

--- src/redux/contacts/contactsOperations.js
+++ src/redux/contacts/contactsOperations.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://6543651701b5e279de2047b2.mockapi.io';
+axios.defaults.baseURL = 'https://6543651701b5e279de2047b2.mockapi.io';
 
 export const fetchContacts = createAsyncThunk(
   'contacts/fetchAll',
